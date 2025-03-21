4542738f3d1baf4019e90fad24be850e7cc0dac9
avisha-singh <b21088@students.iitmandi.ac.in>
2025-03-21 19:45:58 +0530

    Add remove_task function to delete tasks

--- todo.py
+++ todo.py
@@ -10,6 +10,13 @@ def list_tasks():
     else:
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
+            
+def remove_task(task_number):
+    if 0 < task_number <= len(tasks):
+        removed = tasks.pop(task_number - 1)
+        print(f'Task "{removed}" removed.')
+    else:
+        print("Invalid task number.")
 
 if __name__ == "__main__":
     add_task("Finish Assignment")
